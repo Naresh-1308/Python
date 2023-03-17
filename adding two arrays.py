0934379c624b10f3cdd35c73706cbdb7f01e8d90
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-17 12:23:55 +0530

    Create adding two arrays.py

---
+++ adding two arrays.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([2,5,3,7])
+print(arr1+arr2)
+print(arr1[2]) 
+print(arr2[3])
+
+https://colab.research.google.com/drive/1EiN8JNdpmtU5-rd34-PhPBK-7rE4-2wS#scrollTo=X-2LQRSZVLM_

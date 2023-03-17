4c5a427edf43f6de3c9dbed399419a587975014b
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-17 12:10:31 +0530

    Create print numbers in descending order using while.py

---
+++ print numbers in descending order using while.py
@@ -0,0 +1,9 @@
+i = int(input("enter the value of i: "))
+
+while(i>=0):
+  print(i)
+  i=i-1
+  
+  link to Google Colab
+  
+  https://colab.research.google.com/drive/1EiN8JNdpmtU5-rd34-PhPBK-7rE4-2wS#scrollTo=VWd32ybPRUb6

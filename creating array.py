bceea6300e064c51224941afb0c1d13ccb4c8ff1
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-17 12:13:32 +0530

    Create creating array.py

---
+++ creating array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array({1,2,3,4})
+
+print(arr)
+
+Link to Google Colab
+
+https://colab.research.google.com/drive/1EiN8JNdpmtU5-rd34-PhPBK-7rE4-2wS#scrollTo=ydnN6A49JKJU

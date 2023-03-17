c4d7761c3454bfe2919c75ec3f989b63ea501365
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-17 12:46:01 +0530

    Create factorial using while.py

---
+++ factorial using while.py
@@ -0,0 +1,11 @@
+num = int(input("enter the value of n: "))
+r=1
+i=1
+while(i<=num):
+  r = r*i
+  i=i+1
+  print(r)
+  
+  Link to Google Colab
+  
+  https://colab.research.google.com/drive/1EiN8JNdpmtU5-rd34-PhPBK-7rE4-2wS#scrollTo=TKTI1lwZQjOr

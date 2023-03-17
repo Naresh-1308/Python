e90af18a7f92dc804383d943202a55e8cff06e04
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-17 12:14:39 +0530

    Create print random number.py

---
+++ print random number.py
@@ -0,0 +1,7 @@
+import numpy as np
+from numpy.random import random
+random()
+
+Link to Google Colab
+
+https://colab.research.google.com/drive/1EiN8JNdpmtU5-rd34-PhPBK-7rE4-2wS#scrollTo=trVSd4vBJw2f

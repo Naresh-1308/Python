c4de19c5dafa72b1cc9dd0ed5b0ce009a0132b44
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-23 12:14:27 +0530

    Create printing list reverse.py

---
+++ printing list reverse.py
@@ -0,0 +1,10 @@
+i = 0
+length = len(y)
+while(i<length):
+  print(y[length - i-1])
+  i = i + 1
+  
+  
+  Link to GITHUB
+  
+  https://colab.research.google.com/drive/17SaDdLvtAGoZYv5DiR27Ot08rBjt0_1_#scrollTo=sHd5xuIAKm1L

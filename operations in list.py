f5b85b577f485f1b10164bc358546cbefeacbcef
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-23 23:36:17 +0530

    Create operations in list.py

---
+++ operations in list.py
@@ -0,0 +1,19 @@
+bike = ["KTM","KAWASAKI","DUCATI",'SUZUKI',]
+car = ['HONDA','MARUTI SUZUKI', 'TATA', 'HUNDYI','NISSAN','TOYOTA']
+
+bike.append("TVS")
+bike.insert(0,"APACHE")
+car.pop(3)
+
+print(bike)
+print(car)
+
+both =  [bike,car]
+
+print(both)
+
+
+Link to GITHUB
+
+
+https://colab.research.google.com/drive/17SaDdLvtAGoZYv5DiR27Ot08rBjt0_1_#scrollTo=SDhSR0ljFtuw

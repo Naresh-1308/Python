a61990848e42116f23aa5686867bc71107344696
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-23 23:29:16 +0530

    Create reading csv file.py

---
+++ reading csv file.py
@@ -0,0 +1,12 @@
+# read csv file using pandas
+
+import pandas as pd
+
+data = pd.read_csv("student.csv")
+data.head(2)
+data.tail(2)
+
+
+Link to GITHUB
+
+https://colab.research.google.com/drive/17SaDdLvtAGoZYv5DiR27Ot08rBjt0_1_#scrollTo=amR_laXrNvTz

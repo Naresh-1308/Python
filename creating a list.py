148ed9dd8eb5677e562f87421365daffc8c731c7
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-23 23:37:21 +0530

    Create creating a list.py

---
+++ creating a list.py
@@ -0,0 +1,16 @@
+# list is a type of array more specifically it is a type of vector or data structure.
+# list can also be called as the type of the order sequence of the elements or the ordered set of elements,
+# order starts from zero and ends at9...(i other programming language like R list starts from1)
+# list is the storeage element for data types or used to stored mixed data type and everything is numerated.
+
+
+# lets create a list
+
+myfirstlist = [3,4,5,"naresh"]
+print(myfirstlist)
+
+
+Link to GITHUB
+
+
+https://colab.research.google.com/drive/17SaDdLvtAGoZYv5DiR27Ot08rBjt0_1_#scrollTo=icAi9REJFVrr

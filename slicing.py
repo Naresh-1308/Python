1278b739da1f87abd85c704c4edd888e809e8677
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-24 11:46:21 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,22 @@
+# Slicing
+
+# outout hmesha entered index ke ek kam hi aata hai
+
+letters = ['a','b','c','d','e','f']
+letters
+
+letters[1:]
+output - ['b', 'c', 'd', 'e', 'f']
+
+
+letters[:5]
+output - ['a', 'b', 'c', 'd', 'e']
+
+
+letters[1:3]   # outout hmesha entered index ke ek kam hi aata hai
+output - ['b', 'c']
+
+
+LINK TO GITHUB
+
+https://colab.research.google.com/drive/1JgRE1Iuf4rrJRe6dy8qSoFOEDyaegJe6#scrollTo=If75d9JXONIj

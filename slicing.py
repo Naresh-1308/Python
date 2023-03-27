6c41538225a07b2b0892061e1df00b3cf314bff8
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-27 22:57:09 +0530

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -2,19 +2,43 @@
 
 # outout hmesha entered index ke ek kam hi aata hai
 
-letters = ['a','b','c','d','e','f']
+letters = ['a','b','c','d','e','f','g','h']
 letters
 
+output - ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+
+
+length = len(letters)
+length
+
+output - 8 
+
+
+
 letters[1:]
-output - ['b', 'c', 'd', 'e', 'f']
+output - ['b', 'c', 'd', 'e', 'f', 'g', 'h']
+
 
 
 letters[:5]
 output - ['a', 'b', 'c', 'd', 'e']
 
 
+
 letters[1:3]   # outout hmesha entered index ke ek kam hi aata hai
 output - ['b', 'c']
+
+
+
+letters[-4:-1]
+output - ['e', 'f', 'g']
+
+
+
+letters[::-1]
+output - ['h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+
 
 
 LINK TO GITHUB

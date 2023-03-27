6d70a1d860d8dc790227d6cea57a1ae9dabf72c8
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-03-27 22:59:27 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,17 @@
+ #tuple
+
+
+t1 = (24,224,45,56)
+t1
+
+output - (24, 224, 45, 56)
+
+
+t1[2]
+
+output - 45
+
+
+LINK TO GITHUB
+
+https://colab.research.google.com/drive/1JgRE1Iuf4rrJRe6dy8qSoFOEDyaegJe6#scrollTo=8CpF5uInS0rX

f7395251a73b35dac8d91e56daadadcb7569a244
Naresh Gupta <91817586+Naresh1308@users.noreply.github.com>
2023-04-20 12:09:01 +0530

    Create matplotLib_simple_plotting.py

---
+++ matplotLib_simple_plotting.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+plt.plot([2,11,15,40],[4,8,15,22],color='r')
+plt.show()
+
+"or"
+
+import matplotlib.pyplot as plt
+
+plt.plot([2,11,15,40],[4,8,15,22],'rp')
+plt.show()
+
+Link to GITHUB
+
+https://colab.research.google.com/drive/1BU1TryLOiG43NtD1mmFf2yJTf44KSuTu#scrollTo=2MpayiUIYRep
